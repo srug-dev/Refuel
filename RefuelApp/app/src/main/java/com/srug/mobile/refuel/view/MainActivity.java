1222cec0e312f1b7a2814e436a2139f4e1b81625
Sergio de la Rubia <srug86@gmail.com>
2017-02-08 19:03:48 +0100

    Add left DrawerLayout.

--- RefuelApp/app/src/main/java/com/srug/mobile/refuel/view/MainActivity.java
+++ RefuelApp/app/src/main/java/com/srug/mobile/refuel/view/MainActivity.java
@@ -1,7 +1,14 @@
 package com.srug.mobile.refuel.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.srug.mobile.refuel.MVP;
 import com.srug.mobile.refuel.R;
@@ -13,10 +20,15 @@ public class MainActivity extends GenericActivity<MVP.MainRequiredViewOps,
         MainPresenter>
         implements MVP.MainRequiredViewOps {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupActionBar();
+        initializeDrawer();
 
         super.onCreate(MainPresenter.class, this, getIntent().getExtras());
     }
@@ -24,5 +36,54 @@ public class MainActivity extends GenericActivity<MVP.MainRequiredViewOps,
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                switchDrawerLayout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getActionBar();
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.partial_customized_action_bar, null);
+        if (null != actionBar) {
+            actionBar.setCustomView(v);
+        }
+    }
+
+    private void initializeDrawer() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_main);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.activity_main_drawer_layout_opening,
+                R.string.activity_main_drawer_layout_closing) {
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+    }
+
+    private void switchDrawerLayout() {
+        if (mDrawerLayout.isDrawerVisible(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        }
     }
 }
